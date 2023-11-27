9fc30811d9d979c86359e1bb3e949eca041a699c
Viggo K. Helstrup <vkirkeraagrdhelstrup@gmail.com>
2023-11-27 21:48:01 +0100

    the first real commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <Arduino.h>
 
-int 
+const int motor = 13;
+const int sensor = 'A0';
+
+const int thresh= 500;
+const int scantime_s = 3600;
+const int waterring_time_s = 10;
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(motor, OUTPUT);
   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(sensor);
+
+  if (sensorValue < thresh) {
+    digitalWrite(motor, HIGH);
+    delay(waterring_time_s * 1000);
+    digitalWrite(motor, LOW);
+  }
+  delay(scantime_s * 1000);
 }
