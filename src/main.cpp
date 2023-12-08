20db73c1660ee3aba0de077e5cf0488972757225
Viggo K. Helstrup <vkirkegaardhelstrup@gmail.com>
2023-12-08 08:39:11 +0100

    LiquidCrystal_I2C Library for added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,4 @@
 #include <Arduino.h>
-#include <LiquidCrystal.h>
-
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 const int motor = 13;
 const int sensor = A0;
@@ -12,7 +9,6 @@ const int watering_time_s = 10;
 
 void setup() {
   pinMode(motor, OUTPUT);
-  lcd.begin(16, 2);
 }
 
 void loop() {
@@ -24,8 +20,4 @@ void loop() {
   else digitalWrite(motor, LOW);
   
   //delay(scan_time_s * 1000);
-  lcd.setCursor(1, 0);
-  lcd.print("Soil Moisture: ");
-  lcd.setCursor(1, 1);
-  lcd.print(sensorValue);
 }
