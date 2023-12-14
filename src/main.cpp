c52e625bd110dea1d58a6ea889c8ec2553e3279c
Viggo K. Helstrup <vkirkegaardhelstrup@gmail.com>
2023-12-14 08:08:57 +0100

    ui update

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,59 @@
+//include libraries
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 
+//defining constants
+#define second_t 16000000
+#define second 1000
+
+#define motor 13
+#define sensorPin A0
+
+#define thresh  500
+#define scan_time_s 3600
+#define watering_time_s 10
+
+const byte costumchar[8] = {
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000
+};
+
+//defining lcd
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-const int motor = 13;
-const int sensor = A0;
+//defining functions
+void water();
 
-const int thresh = 500;
-const int scan_time_s = 3600;
-const int watering_time_s = 10;
+//defining variables
+int tiksSinceLastScan = 0;
 
 void setup() {
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("Hello, world!"); 
   pinMode(motor, OUTPUT);
 }
 
 void loop() {
-  //int sensorValue = analogRead(sensor);
-  
-  //bool run_motor = (sensorValue < thresh);
-  
-  //if (run_motor)digitalWrite(motor, HIGH);
-  //else digitalWrite(motor, LOW);
-  
-  //delay(scan_time_s * 1000);
+  if(tiksSinceLastScan > scan_time_s * second_t) water();
 
+  lcd.clear();
+  lcd.setCursor(1, 0);
 
+  lcd.print("fugtighed: " + analogRead(sensorPin));
+  
+  tiksSinceLastScan++;
+}
+
+void water() {
+  int sensorValue = analogRead(sensorPin);
+  
+  if (sensorValue < thresh)digitalWrite(motor, HIGH);
+  else digitalWrite(motor, LOW);
 }
